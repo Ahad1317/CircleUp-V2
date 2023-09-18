202a6d9059d2c8145ed1eadf886562114edef654
Ahad1713 <khanazan761@gmail.com>
2023-09-18 12:47:27 +0530

    added comment box

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,9 +5,11 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import { useState } from "react";
 
 const Post = ({post}) => {
-
+    const [commentOpen, setCommentOpen] = useState(false);
     //temporary
     const liked = false;
   return (
@@ -46,6 +48,7 @@ const Post = ({post}) => {
             Share
           </div>
         </div>
+        {commentOpen && <Comments />}
         </div>
     </div>
   )
